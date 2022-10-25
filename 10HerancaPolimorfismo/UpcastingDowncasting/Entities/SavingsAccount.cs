82725c176ecb3f1342ffed95abb36ee88edf8396
Emilio Mazola <mazolatec@gmail.com>
2022-10-24 21:38:05 -0300

    Update UpcastingDowncasting SavingsAccount

---
+++ 10HerancaPolimorfismo/UpcastingDowncasting/Entities/SavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿using AccountBusinessAccount.Entities;
+
+
+namespace UpcastingDowncasting.Entities
+{
+    class SavingsAccount:Account
+    {
+        public double Interestrate { get; set; } //Taxa de juros
+
+        public SavingsAccount(int nunber, string holder, double balance,double interestrate)
+            :base(nunber,holder,balance)
+        {
+            Interestrate = interestrate;
+        }
+        public void UpdateBalance()
+        {
+            Balance += Balance * Interestrate;
+        }
+
+    }
+}
